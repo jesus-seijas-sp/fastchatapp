a723da0e347585314054e3cb693b263abb717267
Jesus Seijas <jseijas@gmail.com>
2022-05-30 15:43:31 +0800

    feat: adding micro WS

--- bench.js
+++ bench.js
@@ -1,5 +1,9 @@
-const autocannon = require("autocannon");
-const corpus = require('./corpus-en.json');
+// eslint-disable-next-line import/no-extraneous-dependencies
+const autocannon = require('autocannon');
+const fs = require('fs');
+const config = require('./conf.json');
+
+const corpus = JSON.parse(fs.readFileSync(config.corpus, 'utf-8'));
 
 const tests = [];
 corpus.data.forEach((item) => {
@@ -8,21 +12,20 @@ corpus.data.forEach((item) => {
   });
 });
 
-const port = process.env.PORT || 8123;
+const port = process.env.PORT || config.port || 3000;
 
 const host = `http://localhost:${port}`;
-const urls = tests.map(x => `${host}/${x.utterance}`);
+
+const urls = tests.map((x) => `${host}/${x.utterance}`);
 
 function startBench() {
-  const instance = autocannon(
-    {
-      url: urls,
-      connections: 400,
-      pipelining: 20,
-      duration: 60,
-      workers: 4
-    }
-  );
+  const instance = autocannon({
+    url: urls,
+    connections: config.connections || 400,
+    pipelining: config.pipelining || 20,
+    duration: config.duration || 60,
+    workers: config.workers || 4,
+  });
   autocannon.track(instance);
 }
 
--- index.js
+++ index.js
@@ -1,32 +1,53 @@
+const useMicroWS = false;
+const uWebSockets = useMicroWS ? require('uwebsockets.js') : undefined;
+const fastify = useMicroWS ? undefined : require('fastify')();
 const cluster = require('cluster');
-const numCPUs = require('os').cpus().length;
-const fastify = require('fastify')();
+const fs = require('fs');
 const { Neural } = require('@agarimo/neural');
-const corpus = require('./corpus-en.json');
 const { getProcessor } = require('@agarimo/languages');
+const os = require('os');
 
+const config = require('./conf.json');
 
-const port = process.env.PORT || process.env.port || 3000;
+const numCPUs = process.env.NUM_CPUS || config.numCPUS || os.cpus().length;
+
+const port = process.env.PORT || process.env.port || config.port || 3000;
 
 function startServer() {
-  const processorEn = getProcessor('en');
+  const corpus = JSON.parse(fs.readFileSync(config.corpus, 'utf-8'));
+  const processorEn = getProcessor(corpus.locale);
   const net = new Neural({ processor: processorEn });
   net.train(corpus);
-  fastify.get('*', function (req, reply) {
-    const input = decodeURI(req.url.slice(1));
-    reply.send(net.run(input));
-  });  
-  fastify.listen(port, '0.0.0.0', () => {
-    console.log(`listening on port ${port}, PID: ${process.pid}`);
-  });
+  if (useMicroWS) {
+    const app = uWebSockets.App();
+    app.get('/:text', (res, req) => {
+      const text = decodeURI(req.getParameter(0));
+      res.end(JSON.stringify(net.run(text)));
+    });
+    app.listen(port, (token) => {
+      if (token) {
+        console.log(`Server listening on port ${port}`);
+      } else {
+        console.log(`Server failed to start on port ${port}`);
+      }
+    });
+  } else {
+    fastify.get('*', (req, reply) => {
+      const input = decodeURI(req.url.slice(1));
+      reply.send(net.run(input)[0].intent);
+    });
+    fastify.listen(port, '0.0.0.0', () => {
+      console.log(`listening on port ${port}, PID: ${process.pid}`);
+    });
+  }
 }
 
 if (cluster.isMaster) {
   console.log(`Master ${process.pid} is running`);
-  for (let i = 0; i < numCPUs; i++) {
+  for (let i = 0; i < numCPUs; i += 1) {
     cluster.fork();
   }
-  cluster.on('exit', worker => {
+  cluster.on('exit', (worker) => {
     console.log(`Worker ${worker.process.pid} died`);
   });
 } else {
